8290f061d6bbe93e393428f01d1de48f278023ff
Jeremiah Miranda <jeremiahmiranda79@gmail.com>
2023-08-29 20:32:35 -0700

    removed package-lock and node modules

---
+++ client/src/utils/queries.js
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_PIZZAS = gql`
+  query getPizzas {
+    pizzas {
+      _id
+      pizzaName
+      foodImage
+      pizzaPrice
+      quantity
+    }
+  }
+`;
