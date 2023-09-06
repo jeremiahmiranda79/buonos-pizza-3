eaca2f6721ec24e257a55469fccf2fbb1db20055
Jeremiah Miranda <jeremiahmiranda79@gmail.com>
2023-09-06 01:03:58 -0700

    last working vialble product

--- client/src/Page/Slider.js
+++ client/src/Page/Slider.js
@@ -35,28 +35,32 @@ import Carousel from 'react-bootstrap/Carousel';
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import slides from './mock.json';
-            function Slider({ sliderData }) {
-    const [index, setIndex] = useState(0);
-    const handleSelect = (selectedIndex, e) => {
-        console.log('selected index: ', selectedIndex)
-        setIndex(selectedIndex);
-    };
+
+function Slider({ sliderData }) {
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+      console.log('selected index: ', selectedIndex)
+      setIndex(selectedIndex);
+  };
+
   return (
     <Carousel activeIndex={index}
-              onSelect={handleSelect}>
-        {slides.map((slide) => (
+      onSelect={handleSelect}>
+      {slides.map((slide) => (
         <Carousel.Item key ={slide.image} interval={slide.interval}>
-        <img
-        className="d-block w-100"
-        src= {slide.image}
-        alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>{slide.title}</h3>
-          <p>{slide.subTitle}</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+          <img
+            className="d-block w-100"
+            src= {slide.image}
+            alt="First slide"
+          />
+
+          <Carousel.Caption>
+            <h3>{slide.title}</h3>
+            <p>{slide.subTitle}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
         ))}
+        
     </Carousel>
   );
 }
