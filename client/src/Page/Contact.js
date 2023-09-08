3250962fc6b58b867917d96101fe61eca627c2f6
charbelk07 <125899348+charbelk07@users.noreply.github.com>
2023-09-07 22:51:01 -0700

    Fixed Bug in Contact page

--- client/src/Page/Contact.js
+++ client/src/Page/Contact.js
@@ -1,51 +1,54 @@
-import React, { useState, useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  const [formState, setFormState] =
-    useState({
-      name: '',
-      email: '',
-      message: '',
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
   });
 
-  const [btnText, setBtnText] = useState ('Submit');
-  const form= useRef();
+  const [btnText, setBtnText] = useState("Submit");
+  const form = useRef();
 
   const handleChange = (e) => {
     e.preventDefault();
-    setFormState({  ...formState, [e.target.name]: e.target.value });
+    setFormState({ ...formState, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formState);
-    emailjs.sendForm(
-      'service_mnv4xud',
-      'template_tbarpys',
-      form.current,
-      'jXiV5LSd3prcbSCXZ')
+    emailjs
+      .sendForm(
+        "service_mnv4xud",
+        "template_tbarpys",
+        form.current,
+        "jXiV5LSd3prcbSCXZ"
+      )
       .then(
         (result) => {
           console.log(result.text);
-          setBtnText ('Message Sent!');
+          setBtnText("Message Sent!");
           setTimeout(() => {
-          window.location.reload();}, 3000);
+            window.location.reload();
+          }, 3000);
         },
         (error) => {
           console.log(error.text);
-          setBtnText ('Error!');
-        });
+          setBtnText("Error!");
+        }
+      );
   };
 
   return (
-    <div className='contact'>
-      <div className='container'>
+    <div className="contact">
+      <div className="container">
         <center>
           <form ref={form} onSubmit={handleSubmit}>
             <div className="mb-3">
               <label htmlFor="name" className="form-label">
-                  Name
+                Name
               </label>
               <input
                 type="text"
@@ -55,43 +58,41 @@ const Contact = () => {
                 defaultValue={formState.name}
                 onChange={handleChange}
               />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="email" className="form-label">
-                    Email address
-                    </label>
-                    <input
-                    type="email"
-                    name="email"
-                    className="form-control"
-                    placeholder="Enter email"
-                    defaultValue={formState.email}
-                    onChange={handleChange}
-                    />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="message" className="form-label">
-                    message
-                    </label>
-                    <textarea
-                    name="message"
-                    className="form-control"
-                    rows="3"
-                    placeholder="Enter message"
-                    defaultValue={formState.message}
-                    onChange={handleChange}
-                    ></textarea>
-                    </div>
-                    <button type="submit" className="btn btn-primary">
-                        {btnText}
-                    </button>
-                    </form>
             </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="Enter email"
+                defaultValue={formState.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="message" className="form-label">
+                message
+              </label>
+              <textarea
+                name="message"
+                className="form-control"
+                rows="3"
+                placeholder="Enter message"
+                defaultValue={formState.message}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              {btnText}
+            </button>
           </form>
         </center>
       </div>
     </div>
   );
-}
+};
 
-export default Contact
+export default Contact;
